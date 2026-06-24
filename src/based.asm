228713c1f79422e7802c6231177296572bd3db96
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-25 01:24:14 +0530

    Adapt `based.asm` to the "new world"

--- src/based.asm
+++ src/based.asm
@@ -1,11 +1,13 @@
+%include "library/syscalls.asm"
 %include "src/core/radix.asm"
-%include "src/linux/syscall.asm"
 
 section .data
     msg db "usage: based <value>", 10               ; message to print, followed by a newline character
     msg_len equ $ - msg                             ; calculate the length of the message. `$` means current address, so `$ - msg` gives the length of the message in bytes
+
     parse_err_msg db "error: invalid input", 10     ; error message for invalid input, followed by a newline character
     parse_err_msg_len equ $ - parse_err_msg         ; calculate the length of the error message
+
     newline db 10                                   ; newline character
 
 section .text
@@ -25,19 +27,13 @@ _start:
     jnz .parse_error        ; if rdx is not zero, jump to parse_error
 
     ; Exit with decimal for testing
-    mov rdi, rax        ; move the parsed value into rdi for exit status
-    mov rax, SYS_EXIT     ; syscall number for exit
-    syscall             ; invoke the syscall to exit with the parsed value
-
-    ; PRINT [rsp+16]      ; print the first argument (argv[1])
-    ; PRINT newline       ; print a newline character
-
-    ; EXIT SUCCESS        ; exit the program with success code
+    mov rdi, rax            ; move the parsed decimal value into rdi for exit code
+    EXIT rdi                ; exit the program with the parsed decimal value as the exit code
 
 .usage:
-    PRINT msg           ; print the usage message
-    EXIT FAILURE        ; exit the program with failure code
+    PRINT msg                   ; print the usage message
+    EXIT EXIT_FAILURE           ; exit the program with failure code
 
 .parse_error:
-    PRINT parse_err_msg ; print the parse error message
-    EXIT FAILURE        ; exit the program with failure code
+    PRINT parse_err_msg         ; print the parse error message
+    EXIT EXIT_FAILURE           ; exit the program with failure code
