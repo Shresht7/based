e810b667e809dae70ad63f7dfdfac6af45e48afc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-25 19:16:29 +0530

    Extract the command-line arguments

--- src/based.c
+++ src/based.c
@@ -1,8 +1,24 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+void print_help()
+{
+    printf("Usage: based <number> <base>\n");
+}
+
 int main(int argc, char *argv[])
 {
-    printf("Hello C!\n");
+    if (argc < 3)
+    {
+        print_help();
+        return 1;
+    }
+
+    char *number_str = argv[1];
+    char *base_str = argv[2];
+
+    printf("Number: %s\n", number_str);
+    printf("Base: %s\n", base_str);
+
     return 0;
 }
